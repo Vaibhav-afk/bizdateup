ebd2686d28f82b282d1fabda66b2432eb5f8a030
Vaibhav-afk <vbhvka@gmail.com>
2022-03-21 21:26:52 +0530

    added show password functionality

--- src/component/1Login.js
+++ src/component/1Login.js
@@ -15,6 +15,8 @@ const Login = () => {
   const [successMessage, setSuccessMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [userDetails, setUserDetails] = useState("");
+  const [viewPwd, setViewPwd] = useState(false);
+
   let history = useHistory();
   const onSubmit = async (data) => {
     console.log(data);
@@ -24,7 +26,7 @@ const Login = () => {
       dispatch({ type: "user", payload: true });
       history.push("/Deals");
       setSuccessMessage("User with the provided credentials found.");
-      localStorage.setItem('loggedIn', true);
+      localStorage.setItem("loggedIn", true);
       setErrorMessage("");
       setUserDetails(response.data);
     } catch (error) {
@@ -33,13 +35,12 @@ const Login = () => {
         console.log("error", error.response.data);
         setErrorMessage(error.response.data);
       }
-      localStorage.setItem('loggedIn', false);
+      localStorage.setItem("loggedIn", false);
     }
   };
 
   return (
     <Form className="input-form" onSubmit={handleSubmit(onSubmit)}>
-      
       <div
         style={{ paddingBottom: "2.2rem" }}
         className="col-md-6 offset-md-3 multi-step-form"
@@ -70,38 +71,52 @@ const Login = () => {
             type="email"
             name="user_email"
             placeholder="Enter registered Email "
-            {...register('user_email',{required: "First name is required.", message :"First name should contain only characters." })}
+            {...register("user_email", {
+              required: "First name is required.",
+              message: "First name should contain only characters.",
+            })}
             className={`${errors ? "input-error" : ""}`}
           />
-          {errors && (
-            <p className="errorMsg">{errors}</p>
-          )}
+          {errors && <p className="errorMsg">{errors}</p>}
         </Form.Group>
 
         <Form.Group controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type="password"
+            type={viewPwd ? "text" : "password"}
             name="user_password"
             placeholder="Enter your password"
-            {...register('user_password',{required: "First name is required.", message :"First name should contain only characters." })}
+            {...register("user_password", {
+              required: "First name is required.",
+              message: "First name should contain only characters.",
+            })}
             className={`${errors ? "input-error" : ""}`}
           />
-          {errors && (
-            <p className="errorMsg">{errors}</p>
-          )}
+          <div
+            style={{
+              position: "absolute",
+              right: "1.4rem",
+              top: "13.4rem",
+              fontSize: "15px",
+              cursor: "pointer",
+            }}
+          >
+            <i
+              onClick={() => {
+                setViewPwd(!viewPwd);
+              }}
+              className={`fa ${viewPwd ? "fa-eye-slash" : "fa-eye"}`}
+            ></i>
+          </div>
+          {errors && <p className="errorMsg">{errors}</p>}
         </Form.Group>
-
-        
 
         <div style={{ textAlign: "center" }}>
           <Button variant="primary" type="submit">
             Login
           </Button>
         </div>
-        <p
-          style={{ fontSize: "15px", textAlign: "center", marginTop: "25px" }}
-        >
+        <p style={{ fontSize: "15px", textAlign: "center", marginTop: "25px" }}>
           Don't have an account yet?{" "}
           <span>
             <Link
